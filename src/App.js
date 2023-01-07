4a5c96b8612e4add188f93792786a2e0430a9c85
yutounun <geek.yuto@gmail.com>
2023-01-07 21:00:40 +0900

    Style expense item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 const Expenseitem = function () {
-  return <h2>Expense item!!</h2>;
+  return (
+    <div className="expense-item">
+      <div>March 3rd</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$200,000</div>
+      </div>
+    </div>
+  );
 };
 
 export default Expenseitem;
