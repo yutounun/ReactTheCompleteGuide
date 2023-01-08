41b80bf90157243d46692d1798947cc089637964
yutounun <geek.yuto@gmail.com>
2023-01-08 10:17:26 +0900

    Add title state to change title reactively

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
+// Since useState exported without "default", it must be embraced with {}
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 import "./ExpenseItem.css";
+import React from "react";
 
 const Expenseitem = function (props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("updated title!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
+      <button onClick={clickHandler}>click me!!</button>
     </Card>
   );
 };
