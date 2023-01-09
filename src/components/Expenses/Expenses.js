000eb7ecd15aa763a273d077b0af0828715a53c6
yutounun <geek.yuto@gmail.com>
2023-01-10 07:27:13 +0900

    Make each expense item dynamic

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,26 +21,13 @@ const Expenses = (props) => {
         />
         {selectedFilterYear}
       </div>
-      <ExpenseItem
-        title={expenses[0].title}
-        price={expenses[0].price}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        price={expenses[1].price}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        price={expenses[2].price}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        price={expenses[3].price}
-        date={expenses[3].date}
-      />
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          price={expense.price}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
