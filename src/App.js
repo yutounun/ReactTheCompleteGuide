de40ef3dc8bf1ca45236738782360f8286f91ee3
yutounun <geek.yuto@gmail.com>
2023-01-09 12:31:57 +0900

    Remove codes I don't need right now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
@@ -25,34 +24,8 @@ const App = () => {
       title: "4th item",
     },
   ];
-  // const [expenses, setExpenses] = useState([
-  //   {
-  //     date: new Date(2022, 1, 1),
-  //     price: 10000,
-  //     title: "1st item",
-  //   },
-  //   {
-  //     date: new Date(2022, 1, 2),
-  //     price: 20000,
-  //     title: "2nd item",
-  //   },
-  //   {
-  //     date: new Date(2022, 1, 3),
-  //     price: 30000,
-  //     title: "3rd item",
-  //   },
-  //   {
-  //     date: new Date(2022, 1, 4),
-  //     price: 40000,
-  //     title: "4th item",
-  //   },
-  // ]);
   const saveExpenseDataHandler = (expenseData) => {
     console.log("expenseData :", expenseData);
-    // const newExpenses = [...expenses, expenseData];
-    // console.log("newExpenses :", newExpenses);
-    // setExpenses(newExpenses);
-    // console.log("expenses :", expenses);
   };
 
   return (
